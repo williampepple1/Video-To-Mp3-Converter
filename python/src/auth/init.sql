57b2998725b0835ec130b86a8176f380fbc3213c
William Pepple <williampepple1@gmail.com>
2023-01-24 08:44:46 +0100

    Update init.sql

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,15 +1,15 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BT 'Pepple12';
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Pepple12';
 
 CREATE DATABASE auth;
 
-GRANT ALL PRIVILEGES ON auth. TO 'auth_user'@'localhost;
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 
 USE auth;
 
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-)
+    password VARCHAR(255) NOT NULL
+);
 
-INSERT INTO user (email, password) VALUES ('william@gmail.com', 'Admin234');
+INSERT INTO user (email, password) VALUES ('william@gmail.com', 'Admin234'); 
